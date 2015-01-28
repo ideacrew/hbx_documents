5b31e5cf5142acdeef52b403b9685ac239906845
Trey Evans <lewis.r.evans@gmail.com>
2015-01-28 14:43:51 -0500

    Working on parsing specs.

--- spec/app/models/parsers/document_list_request_spec.rb
+++ spec/app/models/parsers/document_list_request_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe Parsers::DocumentListRequest do
+  before(:all) do
+    xml_file = File.open(File.join(Padrino.root, "spec", "data", "app/models/parsers/document_list_request.xml")).read
+    @subject = Parsers::DocumentListRequest.parse(xml_file)
+    @identity = @subject.identity
+  end
 
+  it "should have the right name elements" do
+    expect(@identity.name_first).to eq "First"
+    expect(@identity.name_last).to eq "Last"
+  end
+
+  it "should have the right dob" do
+    expect(@identity.dob).to eq "19830125"
+  end
+
+  it "should have the right ssn" do
+    expect(@identity.ssn).to eq "123451234"
+  end
+
+  it "should have the right email" do
+    expect(@identity.email).to eq "email@me.com"
+  end
+
+  it "should have the right member_id" do
+    expect(@identity.hbx_member_id).to eq "12345"
+  end
 end
