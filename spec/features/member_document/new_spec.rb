dce50b02a3786685168c8e95658219ce724cd0e0
Trey Evans <tevans@justplainawful.lan>
2021-02-09 17:12:13 -0500

    Removing incorrect spec.

--- spec/features/member_document/new_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "get /member_documents/new" do
-  it "should have the form" do
-    visit '/member_documents/new'
-    expect(last_response.body.include?("Upload Member Document")).to be_truthy
-  end
-end
